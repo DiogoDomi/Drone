f90be80155a8c8d1b8d3d22e0d273eff6c3ce6d3
Diogo <diogodomidacruz@gmail.com>
2025-09-28 21:57:03 -0300

    Alterando a implementação para utilizar quaternions e processamento do DMP do mpu6050 em vez de fazer calculos via software

--- lib/IMUManager/IMUManager.cpp
+++ lib/IMUManager/IMUManager.cpp
@@ -1,188 +1,127 @@
 #include "IMUManager.h"
-#include <Wire.h>
-#include <Arduino.h>
+#include "I2Cdev.h"
+#include "Wire.h"
+
+namespace { IMUManager* g_pInstance = nullptr; }
+
+void IRAM_ATTR imu_isr_wrapper() {
+    if (g_pInstance) {
+        g_pInstance->dmpDataReady();
+    }
+}
 
 namespace {
-    constexpr uint8_t MPU_ADDR = 0x68;
-    constexpr uint8_t RESET_ADDR = 0x00;
-    constexpr uint8_t POWER_ADDR = 0x6B;
+    enum Axis : uint8_t { X = 0, Y = 1, Z = 2 };
+    enum Angles : uint8_t { YAW = 0, PITCH = 1, ROLL = 2 };
 
-    constexpr uint8_t FILTER_CONFIG = 0x1A;
-    constexpr uint8_t GYRO_CONFIG = 0x1B;
-    constexpr uint8_t ACCEL_CONFIG = 0x1C;
-
-    constexpr uint8_t ACCEL_OUTPUT = 0x3B;
-    constexpr uint8_t I2C_BYTES = 14;
-
-    constexpr uint8_t GYRO_SCALE_250_DEG = 0x00;
-    constexpr uint8_t GYRO_SCALE_500_DEG = 0x08;
-    constexpr uint8_t GYRO_SCALE_1000_DEG = 0x10;
-    constexpr uint8_t GYRO_SCALE_2000_DEG = 0x18;
-
-    constexpr uint8_t ACCEL_SCALE_2_G = 0x00;
-    constexpr uint8_t ACCEL_SCALE_4_G = 0x08;
-    constexpr uint8_t ACCEL_SCALE_8_G = 0x10;
-    constexpr uint8_t ACCEL_SCALE_16_G = 0x18;
-
-    constexpr float GYRO_SENS_250_DEG = 131.0F;
-    constexpr float GYRO_SENS_500_DEG = 65.5F;
-    constexpr float GYRO_SENS_1000_DEG = 32.8F;
-    constexpr float GYRO_SENS_2000_DEG = 16.4F;
-
-    constexpr float ACCEL_SENS_2_G = 16384.0F;
-    constexpr float ACCEL_SENS_4_G = 8192.0F;
-    constexpr float ACCEL_SENS_8_G = 4096.0F;
-    constexpr float ACCEL_SENS_16_G = 2048.0F;
-
-    enum class FilterScale : uint8_t {
-        HZ_260,
-        HZ_184,
-        HZ_94,
-        HZ_44,
-        HZ_21,
-        HZ_10,
-        HZ_5,
-    };
-
+    constexpr uint8_t INTERRUPT_PIN = 14;
+    constexpr uint8_t HARDWARE_LOOPS = 10;
+    constexpr uint16_t SOFTWARE_LOOPS = 200;
     constexpr float MICROS_TO_SEC = 1000000.0F;
-    constexpr float ALPHA = 0.98F;
-
-    constexpr uint16_t WAIT_TIME_US = 10000;
-    constexpr uint16_t CALIB_MIN_READS = 500;
 }
 
-IMUManager::IMUManager() {}
+IMUManager::IMUManager() { g_pInstance = this; }
 
-void IMUManager::setupConfig() {
-    Wire.begin();
-
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(POWER_ADDR);
-    Wire.write(RESET_ADDR);
-    Wire.endTransmission(true);
-
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(FILTER_CONFIG);
-    Wire.write(static_cast<uint8_t>(FilterScale::HZ_260));
-    Wire.endTransmission(true);
-
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(GYRO_CONFIG);
-    Wire.write(GYRO_SCALE_250_DEG);
-    Wire.endTransmission(true);
-
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(ACCEL_CONFIG);
-    Wire.write(ACCEL_SCALE_2_G);
-    Wire.endTransmission(true);
-}
-
-bool IMUManager::readData() {
-    Wire.beginTransmission(MPU_ADDR);
-    Wire.write(ACCEL_OUTPUT);
-    Wire.endTransmission(false);
-    Wire.requestFrom(MPU_ADDR, I2C_BYTES, static_cast<uint8_t>(true));
-
-    if (Wire.available() != 14) {
-        return false;
-    }
-
-    for (uint8_t i = 0; i < 14; i++) {
-        m_i2cBytes[i] = Wire.read();
-    }
-
-    return true;
-}
-
-IMUManager::RawIMUData IMUManager::getRawData() {
-    RawIMUData rawData;
-
-    rawData.accX = static_cast<float>( static_cast<int16_t>( ( (m_i2cBytes[0] << 8) | m_i2cBytes[1]) ) ) / ACCEL_SENS_2_G;
-    rawData.accY = static_cast<float>( static_cast<int16_t>( ( (m_i2cBytes[2] << 8) | m_i2cBytes[3]) ) ) / ACCEL_SENS_2_G;
-    rawData.accZ = static_cast<float>( static_cast<int16_t>( ( (m_i2cBytes[4] << 8) | m_i2cBytes[5]) ) ) / ACCEL_SENS_2_G;
-    rawData.gyrX = static_cast<float>( static_cast<int16_t>( ( (m_i2cBytes[8] << 8) | m_i2cBytes[9]) ) ) / GYRO_SENS_250_DEG;
-    rawData.gyrY = static_cast<float>( static_cast<int16_t>( ( (m_i2cBytes[10] << 8) | m_i2cBytes[11]) ) ) / GYRO_SENS_250_DEG;
-    rawData.gyrZ = static_cast<float>( static_cast<int16_t>( ( (m_i2cBytes[12] << 8) | m_i2cBytes[13]) ) ) / GYRO_SENS_250_DEG;
-
-    return rawData;
-}
-
-bool IMUManager::calibrate() {
-    constexpr uint16_t calibCounter = 1000;
-    uint16_t successfulReads = 0;
-    float accX = 0.0F, accY = 0.0F, accZ = 0.0F;
-    float gyrX = 0.0F, gyrY = 0.0F, gyrZ = 0.0F;
-
-    unsigned long lastLoop = micros();
-
-    for (uint16_t i = 0; i < calibCounter; i++) {
-        while (micros() - lastLoop <= WAIT_TIME_US);
-        lastLoop = micros();
-
-        yield();
-
-        if (readData()) {
-            RawIMUData rawData = getRawData();
-
-            accX += rawData.accX;
-            accY += rawData.accY;
-            accZ += rawData.accZ;
-            gyrX += rawData.gyrX;
-            gyrY += rawData.gyrY;
-            gyrZ += rawData.gyrZ;
-
-            successfulReads++;
-        }
-    }
-
-    if (successfulReads >= CALIB_MIN_READS) {
-        m_offsetAccX = accX / successfulReads;
-        m_offsetAccY = accY / successfulReads;
-        m_offsetAccZ = (accZ / successfulReads) - 1.0F;
-        m_offsetGyrX = gyrX / successfulReads;
-        m_offsetGyrY = gyrY / successfulReads;
-        m_offsetGyrZ = gyrZ / successfulReads;
-
-        return true;
-    }
-
-    return false;
-}                                
-                                 
-void IMUManager::update() {
-    if (!readData()) {
-        return;
-    }
-
-    float accelPitch = 0.0F, accelRoll = 0.0F;
-
-    float elapsedTime = static_cast<float>(micros() - m_previousTime) / MICROS_TO_SEC;
-    m_previousTime = micros();
-
-    RawIMUData rawData = getRawData();
-
-    float accX = rawData.accX - m_offsetAccX;
-    float accY = rawData.accY - m_offsetAccY;
-    float accZ = rawData.accZ - m_offsetAccZ;
-    float gyrX = rawData.gyrX - m_offsetGyrX;
-    float gyrY = rawData.gyrY - m_offsetGyrY;
-    float gyrZ = rawData.gyrZ - m_offsetGyrZ;
-
-    accelPitch = atan2f(accX, accZ) * RAD_TO_DEG;
-    accelRoll = atan2f(-accY, sqrt(accX*accX + accZ*accZ)) * RAD_TO_DEG;
-
-    m_data.pitch = ALPHA * (m_data.pitch + gyrX * elapsedTime) + (1.0F - ALPHA) * accelPitch;
-    m_data.roll = ALPHA * (m_data.roll + gyrY * elapsedTime) + (1.0F - ALPHA) * accelRoll;
-    m_data.yaw += gyrZ * elapsedTime;
-    m_data.elapsedTime = elapsedTime;
-}
-
-IMUData IMUManager::getData() {
-    return m_data;
-}
+void IMUManager::dmpDataReady() { m_interrupt = true; }
 
 void IMUManager::begin() {
     setupConfig();
-    while (!calibrate());
+    calibrateHardwareOffsets();
+    while(!isDMPEnabled()) { yield(); }
+    calibrateSoftwareOffsets();
     m_previousTime = micros();
+}
+
+void IMUManager::setupConfig() {
+    Wire.begin();
+    Wire.setClock(400000);
+    m_mpu.initialize();
+    pinMode(INTERRUPT_PIN, INPUT);
+    m_devStatus = m_mpu.dmpInitialize();
+}
+
+void IMUManager::calibrateHardwareOffsets() {
+    m_mpu.CalibrateAccel(HARDWARE_LOOPS);
+    m_mpu.CalibrateGyro(HARDWARE_LOOPS);
+}
+
+bool IMUManager::isDMPEnabled() {
+    if (m_devStatus != 0) {
+        return false;
+    }
+    m_mpu.setDMPEnabled(true);
+    attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), imu_isr_wrapper, RISING);
+    m_intStatus = m_mpu.getIntStatus();
+    m_dmpReady = true;
+    m_packetSize = m_mpu.dmpGetFIFOPacketSize();
+    return true;
+}
+
+void IMUManager::calibrateSoftwareOffsets() {
+    uint16_t successfulReads = 0;
+    IMUData rawReading{};
+
+    m_swOffsets = IMUData();
+
+    for (uint16_t i = 0; i < SOFTWARE_LOOPS; i++) {
+        while (!m_interrupt) { yield(); }
+
+        if (readRawYPR(rawReading)) {
+            m_swOffsets.yaw += rawReading.yaw;
+            m_swOffsets.pitch += rawReading.pitch;
+            m_swOffsets.roll += rawReading.roll;
+            successfulReads++;
+        }
+    }
+    if (successfulReads > 0) {
+        m_swOffsets.yaw /= successfulReads;
+        m_swOffsets.pitch /= successfulReads;
+        m_swOffsets.roll /= successfulReads;
+    }
+}
+
+bool IMUManager::readRawYPR(IMUData& data) {
+    m_interrupt = false;
+    m_intStatus = m_mpu.getIntStatus();
+    m_fifoCount = m_mpu.getFIFOCount();
+
+    if ((m_intStatus & 0x10) || m_fifoCount == 1024) {
+        m_mpu.resetFIFO();
+        return false;
+    } else if (m_intStatus & 0x02) {
+        if (m_fifoCount < m_packetSize) { return false; }
+        m_mpu.getFIFOBytes(m_fifoBuffer, m_packetSize);
+        m_fifoCount -= m_packetSize;
+
+        m_mpu.dmpGetQuaternion(&m_quaternion, m_fifoBuffer);
+        m_mpu.dmpGetGravity(&m_gravity, &m_quaternion);
+
+        float ypr[3]{};
+        m_mpu.dmpGetYawPitchRoll(ypr, &m_quaternion, &m_gravity);
+
+        data.yaw = ypr[YAW] * 180/M_PI;
+        data.pitch = ypr[PITCH] * 180/M_PI;
+        data.roll = ypr[ROLL] * 180/M_PI;
+
+        return true;
+    }
+    return false;
+}
+
+void IMUManager::update() {
+    if (!m_interrupt) { return; }
+
+    IMUData rawReading{};
+    if (readRawYPR(rawReading)) {
+        m_mpuData.deltaTime = static_cast<float>(micros() - m_previousTime) / MICROS_TO_SEC;
+        m_previousTime = micros();
+
+        m_mpuData.yaw = rawReading.yaw - m_swOffsets.yaw;
+        m_mpuData.pitch = rawReading.pitch - m_swOffsets.pitch;
+        m_mpuData.roll = rawReading.roll - m_swOffsets.roll;
+    }
+}
+
+IMUData IMUManager::getData() {
+    return m_mpuData;
 }
--- lib/IMUManager/IMUManager.h
+++ lib/IMUManager/IMUManager.h
@@ -1,38 +1,39 @@
-#ifndef IMU_MANAGER_H_
-#define IMU_MANAGER_H_
+#ifndef _IMU_MANAGER_H_
+#define _IMU_MANAGER_H_
 
+#include "MPU6050_6Axis_MotionApps20.h"
 #include "IMUData.h"
-#include "stdint.h"
 
 class IMUManager {
     private:
-
-        struct RawIMUData {
-            float accX{}, accY{}, accZ{};
-            float gyrX{}, gyrY{}, gyrZ{};
-        };
+        IMUData m_swOffsets;
+        IMUData m_mpuData{};
 
     private:
 
         unsigned long m_previousTime{};
 
-        float m_offsetAccX{};
-        float m_offsetAccY{};
-        float m_offsetAccZ{};
-        float m_offsetGyrX{};
-        float m_offsetGyrY{};
-        float m_offsetGyrZ{};
+        MPU6050 m_mpu{};
 
-        uint8_t m_i2cBytes[14]{};
+        volatile bool m_interrupt{};
+        bool m_dmpReady{};
+        uint8_t m_intStatus{};
+        uint8_t m_devStatus{};
+        uint16_t m_packetSize{};
+        uint16_t m_fifoCount{};
+        uint8_t m_fifoBuffer[64]{};
 
-        IMUData m_data{};
+        Quaternion m_quaternion{};
+        VectorFloat m_gravity{};
 
     private:
 
-        RawIMUData getRawData();
+        bool readRawYPR(IMUData& data);
+
         void setupConfig();
-        bool readData();
-        bool calibrate();
+        void calibrateHardwareOffsets();
+        void calibrateSoftwareOffsets();
+        bool isDMPEnabled();
 
     public:
 
@@ -40,6 +41,9 @@ class IMUManager {
         void begin();
         void update();
         IMUData getData();
+
+    public:
+        void dmpDataReady();
 
 };
 
