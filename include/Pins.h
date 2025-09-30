dfc3db703ee1b1d025b6e4ad27e8f8cb485e1baa
Diogo <diogodomidacruz@gmail.com>
2025-09-30 20:01:39 -0300

    Atualização no arquivo include/Pins.h: Atualização nos pinos de comunicação I2C do MPU (SCL, SDA e INT); Atualização nos pinos de comunicação serial do GPS (RX); Atualização nos pinos de PWM dos ESCs para os motores.

--- include/Pins.h
+++ include/Pins.h
@@ -6,23 +6,23 @@
 namespace Pins {
     namespace MPU {
         // MPU6050 I2C & INT
-        // constexpr uint8_t SCL_PIN = 5; // D1
-        // constexpr uint8_t SDA_PIN = 4; // D2
-        constexpr uint8_t INTERRUPT_PIN = 14; // D5
+        constexpr uint8_t SCL_PIN = 0;          // D3
+        constexpr uint8_t SDA_PIN = 2;          // D4
+        constexpr uint8_t INTERRUPT_PIN = 4;    // D2
     }
 
     namespace GPS {
         // GPS RX/TX
-        constexpr uint8_t RX_PIN = 0; // D3
-        constexpr int8_t TX_PIN = -1; // Not used
+        constexpr uint8_t RX_PIN = 5;           // D1
+        constexpr int8_t TX_PIN = -1;           // Not used
     }
 
     namespace ESC {
         // ESCs PWM
-        constexpr uint8_t MOTOR_FL_PIN = 14; // D5
-        constexpr uint8_t MOTOR_FR_PIN = 12; // D6
-        constexpr uint8_t MOTOR_BL_PIN = 13; // D7
-        constexpr uint8_t MOTOR_BR_PIN = 15; // D8
+        constexpr uint8_t MOTOR_FL_PIN = 14;    // D5   | -> Motor 1
+        constexpr uint8_t MOTOR_FR_PIN = 12;    // D6  | -> Motor 2
+        constexpr uint8_t MOTOR_BR_PIN = 13;    // D7  | -> Motor 3
+        constexpr uint8_t MOTOR_BL_PIN = 15;    // D8  | -> Motor 4
     }
 };
 
