b5691b738a603411bc490b8a9e27a63488d31aa4
Diogo <diogodomidacruz@gmail.com>
2025-10-08 15:27:15 -0300

    Update: Atualizando pinos
    -Atualizando pinagem para cada motor para facilitar a solda na PCI.

--- include/Pins.h
+++ include/Pins.h
@@ -6,8 +6,8 @@
 namespace Pins {
     namespace MPU {
         // MPU6050 I2C & INT
-        constexpr uint8_t SCL_PIN = 0;          // D3
-        constexpr uint8_t SDA_PIN = 2;          // D4
+        constexpr uint8_t SCL_PIN = 2;          // D4
+        constexpr uint8_t SDA_PIN = 0;          // D3
         constexpr uint8_t INTERRUPT_PIN = 4;    // D2
     }
 
@@ -19,10 +19,10 @@ namespace Pins {
 
     namespace ESC {
         // ESCs PWM
-        constexpr uint8_t MOTOR_FL_PIN = 14;    // D5   | -> Motor 1
-        constexpr uint8_t MOTOR_FR_PIN = 12;    // D6  | -> Motor 2
-        constexpr uint8_t MOTOR_BR_PIN = 13;    // D7  | -> Motor 3
-        constexpr uint8_t MOTOR_BL_PIN = 15;    // D8  | -> Motor 4
+        constexpr uint8_t MOTOR_FL_PIN = 15;    // D8  | -> Motor 1
+        constexpr uint8_t MOTOR_FR_PIN = 14;    // D5  | -> Motor 2
+        constexpr uint8_t MOTOR_BR_PIN = 12;    // D6  | -> Motor 3
+        constexpr uint8_t MOTOR_BL_PIN = 13;    // D7  | -> Motor 4
     }
 };
 
