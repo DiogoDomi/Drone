e2bc42ec9849eacc0bff0332c8c2730f99de84c8
Diogo <diogodomidacruz@gmail.com>
2025-10-25 19:00:05 -0300

    Update: Adicionando correções de motores e PID.
    -Escs com firmwares diferentes causando diferença de potência com o
    mesmo sinal no drone. Para isso foi adicionado a constante
    'BACK_CORRECTION' para tentar corrigir essa diferença via software;
    -Tempo de delay do debug de MPU/Joystick/PID diminuido para 1 (quase sem
    delay nenhum);
    -Lógica de output dos motores corrigidas de acordo com o PID e não com a
    entrada dos joystick/mpu.

--- lib/FlightManager/FlightManager.cpp
+++ lib/FlightManager/FlightManager.cpp
@@ -19,7 +19,9 @@ namespace {
     constexpr float PITCH_PID_SCALE = 1.0F;
     constexpr float ROLL_PID_SCALE = 1.0F;
 
-    constexpr uint8_t DEBUG_PRINT_INTERVAL = 100;
+    constexpr uint8_t DEBUG_PRINT_INTERVAL = 1;
+
+    constexpr float BACK_CORRECTION = 0.95;
 }
 
 FlightManager::FlightManager(IMUManager& imu) :
@@ -100,10 +102,10 @@ void FlightManager::writeMotors() {
     float scaledPitch = m_pitchPidOutput * PITCH_PID_SCALE;
     float scaledRoll = m_rollPidOutput * ROLL_PID_SCALE;
 
-    float motor_FL_F = static_cast<float>(m_throttleMap) - scaledYaw - scaledPitch + scaledRoll;
-    float motor_FR_F = static_cast<float>(m_throttleMap) + scaledYaw - scaledPitch - scaledRoll;
-    float motor_BR_F = static_cast<float>(m_throttleMap) - scaledYaw + scaledPitch - scaledRoll;
-    float motor_BL_F = static_cast<float>(m_throttleMap) + scaledYaw + scaledPitch + scaledRoll;
+    float motor_FL_F = static_cast<float>(m_throttleMap) + scaledPitch - scaledRoll + scaledYaw;
+    float motor_FR_F = static_cast<float>(m_throttleMap) + scaledPitch + scaledRoll - scaledYaw;
+    float motor_BR_F = (static_cast<float>(m_throttleMap) * BACK_CORRECTION) - scaledPitch + scaledRoll + scaledYaw;
+    float motor_BL_F = (static_cast<float>(m_throttleMap) * BACK_CORRECTION) - scaledPitch - scaledRoll - scaledYaw;
     
     uint16_t motor_FL = static_cast<uint16_t>(constrain(motor_FL_F, Pwm::IDLE, Pwm::MAX));
     uint16_t motor_FR = static_cast<uint16_t>(constrain(motor_FR_F, Pwm::IDLE, Pwm::MAX));
