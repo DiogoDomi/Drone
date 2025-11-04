5e764728805a40f4dc40d39118ddad26c7409789
Diogo <diogodomidacruz@gmail.com>
2025-11-04 15:40:46 -0300

    Update: Atualizando implementação da API.
    -Removendo toda a lógica de verificação para validar se os dados são
    diferentes;
    -Adicionando membro de referência para API FlightManager para obtenção do
    membro state para telemetria;
    -Modificação dos métodos update() e isTelemetryValid().

--- lib/TelemetryManager/TelemetryManager.cpp
+++ lib/TelemetryManager/TelemetryManager.cpp
@@ -2,52 +2,26 @@
 #include "Flags.h"
 #include <cmath>
 
-TelemetryManager::TelemetryManager(WiFiManager& wifi, GPSManager& gps) :
+TelemetryManager::TelemetryManager(WiFiManager& wifi, GPSManager& gps, FlightManager& flight) :
     m_wifi(wifi),
-    m_gps(gps)
+    m_gps(gps),
+    m_flight(flight)
     {}
 
 void TelemetryManager::update() {
-    m_currentTelemetry.rssi = m_wifi.getRSSIData();
-    m_currentTelemetry.gps = m_gps.getGPSData();
-
-    m_currentTelemetry.isValid = isTelemetryValid(m_currentTelemetry);
+    m_telemetry.state = static_cast<uint8_t>(m_flight.getStateData());
+    m_telemetry.rssi = m_wifi.getRSSIData();
+    m_telemetry.gps = m_gps.getGPSData();
+    m_telemetry.isValid = isTelemetryValid(m_telemetry);
 }
 
-bool TelemetryManager::shouldSendToWeb() {
-    if (isTelemetryDifferent(m_previousWebTelemetry, m_currentTelemetry)) {
-        m_previousWebTelemetry = m_currentTelemetry;
-        return true;
-    }
-    return false;
-}
-
-bool TelemetryManager::shouldSendToBD() {
-    if (isTelemetryDifferent(m_previousDBTelemetry, m_currentTelemetry) &&
-    isTelemetryValid(m_currentTelemetry)) {
-        m_previousDBTelemetry = m_currentTelemetry;
-        return true;
-    }
-    return false;
-}
-
-const TelemetryData& TelemetryManager::getTelemetryData() const {
-    return m_currentTelemetry;
+const TelemetryData& TelemetryManager::getTelemetry() const {
+    return m_telemetry;
 }
 
 bool TelemetryManager::isTelemetryValid(const TelemetryData& telemetry) const {
     if (telemetry.rssi == Flags::WIFI_RSSI_INVALID) { return false; }
     if (telemetry.gps.lat == Flags::GPS_INVALID_LOCATION) { return false; }
+    if (telemetry.gps.alt == Flags::GPS_INVALID_ALTITUDE) { return false; }
     return true;
 }
-
-bool TelemetryManager::isTelemetryDifferent(const TelemetryData& current, const TelemetryData& previous) const {
-    constexpr float EPSILON = 0.000001F;
-
-    if (current.rssi != previous.rssi) { return true; }
-    if (fabsf(current.gps.lat - previous.gps.lat) > EPSILON) { return true; }
-    if (fabsf(current.gps.lon - previous.gps.lon) > EPSILON) { return true; }
-    if (fabsf(current.gps.alt - previous.gps.alt) > EPSILON) { return true; }
-
-    return false;
-};
--- lib/TelemetryManager/TelemetryManager.h
+++ lib/TelemetryManager/TelemetryManager.h
@@ -4,29 +4,26 @@
 #include "WiFiManager.h"
 #include "GPSManager.h"
 #include "TelemetryData.h"
+#include "FlightManager.h"
 
 class TelemetryManager {
     private:
 
         WiFiManager& m_wifi;
         GPSManager& m_gps;
+        FlightManager& m_flight;
 
-        TelemetryData m_currentTelemetry{};
-        TelemetryData m_previousWebTelemetry{};
-        TelemetryData m_previousDBTelemetry{};
+        TelemetryData m_telemetry{};
 
     private:
 
         bool isTelemetryValid(const TelemetryData& telemetry) const;
-        bool isTelemetryDifferent(const TelemetryData& current, const TelemetryData& previous) const;
 
     public:
 
-        TelemetryManager(WiFiManager& wifi, GPSManager& gps);
+        TelemetryManager(WiFiManager& wifi, GPSManager& gps, FlightManager& flight);
         void update();
-        bool shouldSendToWeb();
-        bool shouldSendToBD();
-        const TelemetryData& getTelemetryData() const;
+        const TelemetryData& getTelemetry() const;
 };
 
 #endif
